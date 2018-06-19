e73f60146edc8d605d08f5eaa1fa0dc49e99b44b
SgtSeppel <SgtSeppel@users.noreply.github.com>
2018-06-19 21:52:57 +0200

    Added dwd-parser.py
    Added first version of parser for the german DWD (Deutscher Wetterdienst)

---
+++ sdk-parsers/RMParserFramework/parsers/dwd-parser.py
@@ -0,0 +1,73 @@
+from RMParserFramework.rmParser import RMParser  # Mandatory include for parser definition
+from RMUtilsFramework.rmLogging import log       # Optional include for logging
+
+import csv
+
+from RMUtilsFramework.rmTimeUtils import rmTimestampFromDateAsString
+
+
+class ExampleParser(RMParser):
+    parserName = "DWD Parser"  # Your parser name
+    parserDescription = "Parser for the german \"Deutscher Wetterdienst\""  # A short description of your parser
+    parserForecast = True # True if parser provides future forecast data
+    parserHistorical = False # True if parser also provides historical data (only actual observed data)
+    parserInterval = 6 * 3600             # Your parser running interval in seconds, data will only be mixed in hourly intervals
+    parserDebug = False
+    params = {"station": None}
+
+    def perform(self):                # The function that will be executed must have this name
+
+        station = self.params.get("station", None)
+        if station is None:
+            log.debug("No station set, using Frankfurt am Main" % datestring)
+            station = 10637
+        url = "http://opendata.dwd.de/weather/local_forecasts/poi/" + str(station) + "-MOSMIX.csv"
+
+        URLParams = [
+            ("User-Agent", "RainMachine v2")
+        ]
+
+        try:
+
+            file = self.openURL(url, URLParams)
+            if file is None:
+                return
+
+            reader = csv.reader(file, delimiter=';')
+            included_cols = [0, 1, 2, 3, 4, 5, 9, 14, 22, 31, 34]
+            next(reader)
+            next(reader)
+            next(reader)
+            for row in reader:
+                content = list(row[i] for i in included_cols)
+                #print(content)
+                datestring = content[0]+':'+content[1]
+                timestamp = rmTimestampFromDateAsString(datestring, '%d.%m.%y:%H:%M')
+                if timestamp is None:
+                    log.debug("Cannot convert timestamp: %s to unix timestamp" % datestring)
+                    continue
+                #print timestamp
+                self.addValue(RMParser.dataType.TEMPERATURE, timestamp, float(content[2].replace(",", ".")))
+                self.addValue(RMParser.dataType.DEWPOINT, timestamp, float(content[3].replace(",", ".")))
+                if content[4] != '---':
+                    self.addValue(RMParser.dataType.MAXTEMP, timestamp, float(content[4].replace(",", ".")))
+                if content[5] != '---':
+                    self.addValue(RMParser.dataType.MINTEMP, timestamp, float(content[5].replace(",", ".")))
+                ## km/h -> m/s
+                self.addValue(RMParser.dataType.WIND, timestamp, 0.27777777777778 * float(content[6].replace(",", ".")))
+                self.addValue(RMParser.dataType.QPF, timestamp, float(content[7].replace(",", ".")))
+                if content[8] != '---':
+                    self.addValue(RMParser.dataType.SOLARRADIATION, timestamp, float(content[8].replace(",", ".")))
+                self.addValue(RMParser.dataType.PRESSURE, timestamp, float(content[9].replace(",", "."))/10)
+                if content[10] != '---':
+                    self.addValue(RMParser.dataType.SOLARRADIATION, timestamp, float(content[10].replace(",", ".")))
+
+
+        except Exception, e:
+            log.error("*** Error running OpenWeatherMap parser")
+            log.exception(e)
+
+
+if __name__ == "__main__":
+    p = ExampleParser()
+    p.perform()
