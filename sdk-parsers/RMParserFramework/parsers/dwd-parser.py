3f70b24f03108f7d52626113b0c147898ffaa404
SgtSeppel <SgtSeppel@users.noreply.github.com>
2018-06-22 07:52:02 +0200

    Update dwd-parser.py
    - Added GPL header to clarify that the code can be modified and distributed by Rainmachine.

--- sdk-parsers/RMParserFramework/parsers/dwd-parser.py
+++ sdk-parsers/RMParserFramework/parsers/dwd-parser.py
@@ -1,3 +1,19 @@
+# DWD parser for rainmachine
+# Copyright (C) 2018  Sebastian Kuhn
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from RMParserFramework.rmParser import RMParser  # Mandatory include for parser definition
 from RMUtilsFramework.rmLogging import log       # Optional include for logging
 
