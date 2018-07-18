a6eacc43f12eeb46d60c49e2d700b9badb82cb23
Nicu Pavel <npavel@mini-box.com>
2018-07-18 01:43:34 -0700

    DWD Parser small fixes. Disabled SOLAR RADIATION

--- sdk-parsers/RMParserFramework/parsers/dwd-parser.py
+++ sdk-parsers/RMParserFramework/parsers/dwd-parser.py
@@ -22,20 +22,22 @@ import csv
 from RMUtilsFramework.rmTimeUtils import rmTimestampFromDateAsString
 
 
-class ExampleParser(RMParser):
+class DWD(RMParser):
     parserName = "DWD Parser"
-    parserDescription = "Parser for the german \"Deutscher Wetterdienst\""
+    parserDescription = "Parser for the german \"Deutscher Wetterdienst\". To get station ID please see: https://www.dwd.de/DE/leistungen/opendata/help/stationen/mosmix_stationskatalog.cfg?view=nasPublication&nn=16102"
     parserForecast = True
-    parserHistorical = False
+    parserHistorical = True
     parserInterval = 6 * 3600
     parserDebug = False
     params = {"station": None}
+    defaultParams = {"station": "10637"}
 
     def perform(self):
         station = self.params.get("station", None)
-        if station is None:
-            log.debug("No station set, using Frankfurt am Main" % datestring)
-            station = 10637
+        if station is None or station == "":
+            station = "10637"
+            log.debug("No station set, using Frankfurt am Main (%s)" % station)
+
         url = "http://opendata.dwd.de/weather/local_forecasts/poi/" + str(station) + "-MOSMIX.csv"
 
         URLParams = [
@@ -45,6 +47,7 @@ class ExampleParser(RMParser):
         try:
             file = self.openURL(url, URLParams)
             if file is None:
+                self.lastKnownError = "Cannot read data from DWD Service."
                 return
             reader = csv.reader(file, delimiter=';')
             included_cols = [0, 1, 2, 3, 4, 5, 9, 14, 22, 31, 34]
@@ -60,27 +63,33 @@ class ExampleParser(RMParser):
                     log.debug("Cannot convert timestamp: %s to unix timestamp" % datestring)
                     continue
                 #print timestamp
-                self.addValue(RMParser.dataType.TEMPERATURE, timestamp, float(content[2].replace(",", ".")))
-                self.addValue(RMParser.dataType.DEWPOINT, timestamp, float(content[3].replace(",", ".")))
+                if content[2] != '---':
+                    self.addValue(RMParser.dataType.TEMPERATURE, timestamp, float(content[2].replace(",", ".")))
+                if content[3] != '---':
+                    self.addValue(RMParser.dataType.DEWPOINT, timestamp, float(content[3].replace(",", ".")))
                 if content[4] != '---':
                     self.addValue(RMParser.dataType.MAXTEMP, timestamp, float(content[4].replace(",", ".")))
                 if content[5] != '---':
                     self.addValue(RMParser.dataType.MINTEMP, timestamp, float(content[5].replace(",", ".")))
                 ## km/h -> m/s
-                self.addValue(RMParser.dataType.WIND, timestamp, 0.27777777777778 * float(content[6].replace(",", ".")))
-                self.addValue(RMParser.dataType.QPF, timestamp, float(content[7].replace(",", ".")))
-                if content[8] != '---':
-                    self.addValue(RMParser.dataType.SOLARRADIATION, timestamp, float(content[8].replace(",", ".")))
-                self.addValue(RMParser.dataType.PRESSURE, timestamp, float(content[9].replace(",", "."))/10)
+                if content[6] != '---':
+                    self.addValue(RMParser.dataType.WIND, timestamp, 0.27777777777778 * float(content[6].replace(",", ".")))
+                if content[7] != '---':
+                    self.addValue(RMParser.dataType.QPF, timestamp, float(content[7].replace(",", ".")))
+
+                #if content[8] != '---':
+                #    self.addValue(RMParser.dataType.SOLARRADIATION, timestamp, float(content[8].replace(",", ".")))
+
+                if content[9] != '---':
+                    self.addValue(RMParser.dataType.PRESSURE, timestamp, float(content[9].replace(",", "."))/10)
                 if content[10] != '---':
                     self.addValue(RMParser.dataType.SOLARRADIATION, timestamp, float(content[10].replace(",", ".")))
 
-
         except Exception, e:
-            log.error("*** Error running OpenWeatherMap parser")
+            log.error("*** Error running DWD parser")
             log.exception(e)
 
 
 if __name__ == "__main__":
-    p = ExampleParser()
+    p = DWDParser()
     p.perform()
