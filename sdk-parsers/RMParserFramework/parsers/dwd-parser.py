b5e86685c4315584eeacb22ed3aecd02ca863cad
SgtSeppel <SgtSeppel@users.noreply.github.com>
2018-09-10 12:47:53 +0200

    Update dwd-parser.py
    - removed solar radiation
    - fixed conversion of windspeed
    - fixed classname when running the parser locally

--- sdk-parsers/RMParserFramework/parsers/dwd-parser.py
+++ sdk-parsers/RMParserFramework/parsers/dwd-parser.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # DWD parser for rainmachine
 # Copyright (C) 2018  Sebastian Kuhn
 
@@ -14,40 +15,41 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# coding=utf-8
-
 from RMParserFramework.rmParser import RMParser  # Mandatory include for parser definition
 from RMUtilsFramework.rmLogging import log       # Optional include for logging
+from RMUtilsFramework.rmTimeUtils import rmNowDateTime, rmGetStartOfDay # helpers for date calculations
+from RMUtilsFramework.rmTimeUtils import rmTimestampFromDateAsString # helper for timestamp creation
+from RMUtilsFramework.rmUtils import distanceBetweenGeographicCoordinatesAsKm
+import csv  # needed for csv import
+import urllib2
 
-import csv
-
-from RMUtilsFramework.rmTimeUtils import rmTimestampFromDateAsString
-
-
-class ExampleParser(RMParser):
+class DWDParser(RMParser):
     parserName = "DWD Parser"
     parserDescription = "Parser for the german \"Deutscher Wetterdienst\""
     parserForecast = True
-    parserHistorical = False
+    parserHistorical = True
     parserInterval = 6 * 3600
     parserDebug = False
     params = {"station": None}
+    defaultParams = {"station": 10637}
 
     def perform(self):
         station = self.params.get("station", None)
-        if station is None:
-            log.debug("No station set, using Frankfurt am Main")
-            station = 10637
-        url = "http://opendata.dwd.de/weather/local_forecasts/poi/" + str(station) + "-MOSMIX.csv"
+        if station is None or station == "":
+            url = "http://opendata.dwd.de/weather/local_forecasts/poi/10637-MOSMIX.csv"
+            log.debug("No station set, using Frankfurt am Main (10637)")
+        else:
+            url = "http://opendata.dwd.de/weather/local_forecasts/poi/" + str(station) + "-MOSMIX.csv" 
 
         URLParams = [
             ("User-Agent", "RainMachine v2")
         ]
 
+        # read weather data for the given station
         try:
-
             file = self.openURL(url, URLParams)
             if file is None:
+                self.lastKnownError = "Cannot read data from DWD Service."
                 return
 
             reader = csv.reader(file, delimiter=';')
@@ -60,6 +62,7 @@ class ExampleParser(RMParser):
                 #print(content)
                 datestring = content[0]+':'+content[1]
                 timestamp = rmTimestampFromDateAsString(datestring, '%d.%m.%y:%H:%M')
+                yesterdayTimestamp = rmGetStartOfDay(timestamp - 12 * 60 * 60)
                 if timestamp is None:
                     log.debug("Cannot convert timestamp: %s to unix timestamp" % datestring)
                     continue
@@ -72,14 +75,14 @@ class ExampleParser(RMParser):
 
                 # Max Temperature
                 if content[4] != '---':
-                    self.addValue(RMParser.dataType.MAXTEMP, timestamp, float(content[4].replace(",", ".")))
+                    self.addValue(RMParser.dataType.MAXTEMP, yesterdayTimestamp, float(content[4].replace(",", ".")))
 
                 # Min Temperature
                 if content[5] != '---':
-                    self.addValue(RMParser.dataType.MINTEMP, timestamp, float(content[5].replace(",", ".")))
+                    self.addValue(RMParser.dataType.MINTEMP, yesterdayTimestamp, float(content[5].replace(",", ".")))
 
                 # Wind speed km/h -> m/s
-                self.addValue(RMParser.dataType.WIND, timestamp, float(content[6].replace(",", "."))/3.6)
+                self.addValue(RMParser.dataType.WIND, timestamp, 0.27777777777778 * float(content[6].replace(",", ".")))
 
                 # precipitation amount last hour in mm
                 self.addValue(RMParser.dataType.QPF, timestamp, float(content[7].replace(",", ".")))
@@ -87,15 +90,10 @@ class ExampleParser(RMParser):
                 # atmospheric pressure
                 self.addValue(RMParser.dataType.PRESSURE, timestamp, float(content[9].replace(",", "."))/10)
 
-                # Solar radiation kJ/m² => MJ/m²
-                if content[10] != '---':
-                    self.addValue(RMParser.dataType.SOLARRADIATION, timestamp, float(content[10].replace(",", "."))/10)
-
         except Exception, e:
             log.error("*** Error running DWD parser")
             log.exception(e)
 
-
 if __name__ == "__main__":
-    p = ExampleParser()
+    p = DWDParser()
     p.perform()
