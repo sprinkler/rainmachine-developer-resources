d990d15fbba9fef7819e8c40a29346baeaff3ea1
ghgifford <34146668+ghgifford@users.noreply.github.com>
2020-07-06 05:29:10 -0500

    Updated for TEMPEST device compatibility (#19)
    - Added ability to capture data from either TEMPEST or Air+Sky weather station. 
     - Correction: Wind_Tot counter was not being reset on new day.    
     - Update to send yesterday's end of day results only once.

--- sdk-parsers/RMParserFramework/parsers/wf-parser.py
+++ sdk-parsers/RMParserFramework/parsers/wf-parser.py
@@ -2,6 +2,14 @@
 # All rights reserved.
 # Author: Bob Paauwe <bpaauwe@bobsplace.com>
 #
+# Version 1.2.0   06/30/2020  (G.H. Gifford)
+#  - Added ability to capture data from either TEMPEST or Air+Sky weather station. 
+#      - updates Device Type, Serial Num, and UPD data indexes
+#      - don't use with both TEMPEST and Air+Sky devices. Rain totals will be 2x actual. 
+#  - Correction: Wind_Tot counter was not being reset on new day.    
+#  - Update to send yesterday's end of day results only once.
+#
+#
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
 # to deal in the Software without restriction, including without limitation
@@ -42,28 +50,32 @@ import time as mod_time
 
 class WeatherFlow(RMParser):
     parserName = "WeatherFlow Parser"
-    parserDescription = "Parse data from a WeatherFlow Smart Weather Station"
+    parserDescription = "Parse data from a WeatherFlow Smart Weather Station (ver 1.2.0)"
     parserForecast = False # True if parser provides future forecast data
     parserHistorical = True # True if parser also provides historical data (only actual observed data)
     # parserInterval defines how often the mixer will call this to retrieve
     # data. The default is set to 300 seconds.  It should not be set to
     # less than 60 seconds as the WeatherFlow data is only updated once a
     # minute.
-    parserInterval = 300             # Your parser running interval in seconds, data will only be mixed in hourly intervals
+    parserInterval = 600        # 10min - Higher freq to record rain in case of pwr outage. Your parser running interval in seconds, 
+                                #         data will only be mixed in hourly intervals
     #parserDebug = False
     parserDebug = True
     parserEnabled = True
     parserData = []
     started = False
+    newDay = 0
 
     # Users must supply the sensor serial numbers
     params = {
-        "AirSerialNumber": None,
-        "SkySerialNumber": None
+        "AirSerialNumber" : "AR-00000000",           
+        "SkySerialNumber" : "SK-00000000",          
+        "TempestSerialNum": "ST-00000000"           
     }
     defaultParams = {
-        "AirSerialNumber": None,
-        "SkySerialNumber": None
+        "AirSerialNumber" : "AR-00000000",
+        "SkySerialNumber" : "SK-00000000",
+        "TempestSerialNum": "ST-00000000"
     }
 
     parserData = [dict() for x in range(2)]
@@ -71,7 +83,7 @@ class WeatherFlow(RMParser):
     def __init__(self):
         RMParser.__init__(self)
         self.started = False
-        log.debug("WeatherFlow local parser 1.1.3  08/15/2019")
+        log.info("Initializing WeatherFlow local UDP parser (ver 1.2.0)")
 
     def perform(self):                # The function that will be executed must have this name
 
@@ -112,8 +124,18 @@ class WeatherFlow(RMParser):
             # TODO: How do we stop the thread once it's started?
             threading.Thread(target = self.wfUDPData).start()
             return  None # First time, just start the thread, we have no data yet.
-        for i, rawdata in enumerate(self.parserData, start=0):
+
+            
+        for idx, rawdata in enumerate(self.parserData):
             if 'report' in rawdata:
+                logMsg = "Interval Summary:"
+                if idx == 1:               # if looking at yesterday data (0 = today, 1 = yesterday) ...
+                    if self.newDay :       # Check if need to send yesterday's data
+                        self.newDay = 0    # reset the flag and send yesterday's data   
+                        logMsg = "Yesterday's EoD Summary:" 
+                    else:                  # otherwise skip sending it, only want to send one time 
+                        continue
+
                 self.addValue(RMParser.dataType.TEMPERATURE, rawdata['ts'], rawdata['report']['temperature'])
                 self.addValue(RMParser.dataType.RH, rawdata['ts'], rawdata['report']['humidity'])
                 self.addValue(RMParser.dataType.PRESSURE, rawdata['ts'], rawdata['report']['pressure'])
@@ -127,6 +149,13 @@ class WeatherFlow(RMParser):
                     self.addValue(RMParser.dataType.MINTEMP, rawdata['ts'], rawdata['report']['min_temp'])
                 self.addValue(RMParser.dataType.MAXRH, rawdata['ts'], rawdata['report']['max_humid'])
                 self.addValue(RMParser.dataType.MINRH, rawdata['ts'], rawdata['report']['min_humid'])
+
+                
+                              
+                log.info("%s temp(C,F): %.2f / %.2f, wind(m/s,mph): %.2f / %.2f, rain_dayTot(mm,in): %.2f / %.2f" % (logMsg, 
+                        rawdata['report']["temperature"], ((rawdata['report']["temperature"] * 9 / 5) + 32), 
+                        rawdata['report']["wind"], (rawdata['report']["wind"] * 2.237), 
+                        rawdata['report']["rain"], (rawdata['report']["rain"] / 25.4) ))
 
                 log.debug("timestamp = %s" % datetime.fromtimestamp(rawdata['ts']))
                 log.debug("temperature = %f" % rawdata['report']["temperature"])
@@ -142,6 +171,7 @@ class WeatherFlow(RMParser):
                 log.debug("min humidity = %f" % rawdata['report']["min_humid"])
                 log.debug("")
 
+               
 
     # Listen for and process WeatherFlow data that is broadcast on UDP port 50222
     def wfUDPData(self):
@@ -154,10 +184,6 @@ class WeatherFlow(RMParser):
         srad_total = 0
         rain_total = 0
         dewp_total = 0
-        prev_air_hour = 0
-        prev_sky_hour = 0
-        prev_air_day = 0
-        prev_sky_day = 0
         bufferSize = 1024 # whatever you need
         port = 50222
         day_of_year = 0
@@ -185,9 +211,10 @@ class WeatherFlow(RMParser):
                 temp_total = 0
                 humd_total = 0
                 pres_total = 0
-                dewp_total = 0
+                wind_total = 0
                 srad_total = 0
                 rain_total = 0
+                dewp_total = 0
                 self.report = {
                         'temperature': 0,
                         'humidity': 0,
@@ -204,61 +231,97 @@ class WeatherFlow(RMParser):
 
                 day_of_year = now.timetuple().tm_yday
                 self.parserData[1] = self.parserData[0]
+ 
+                self.newDay +=1            # signal that just rolled into a new day, need to send a yesterday summary onetime
 
                 # reset yesterday's timestamp to start of day
                 if 'ts' in self.parserData[1]:
                     self.parserData[1]['ts'] = rmGetStartOfDay(self.parserData[1]['ts'])
 
-            #log.debug("type = %s s/n = %s" % (data["type"], data["serial_number"]))
+            #log.debug("type = %s broadcast s/n = %s  TEMPEST target: %s" % (data["type"], data["serial_number"], self.params["TempestSerialNum"]))
 
-            if (data["type"] == "obs_air") and (data["serial_number"] == self.params["AirSerialNumber"]):
+            debugMsg = "Observation: "
+
+            if  (   ((data["type"] == "obs_air") and (data["serial_number"] == self.params["AirSerialNumber"])) or
+                    ((data["type"] == "obs_st") and (data["serial_number"] == self.params["TempestSerialNum"])) ):
+
+                if   data["type"] == "obs_air":                  
+                    pres_idx = 1                # UDP packet data indexes for Air device
+                    temp_idx = 2
+                    humd_idx = 3
+                else:
+                    pres_idx = 6                # UDP packet data indexes for TEMPEST device
+                    temp_idx = 7
+                    humd_idx = 8
 
                 air_count += 1
 
-                temp_total += data["obs"][0][2]
+                temp_total += data["obs"][0][temp_idx]
                 self.report["temperature"] = float(temp_total) / float(air_count)
 
-                humd_total += data["obs"][0][3]
+                humd_total += data["obs"][0][humd_idx]
                 self.report["humidity"] = float(humd_total) / float(air_count)
 
                 # report pressure in hpa so convert from mb to hpa
-                pres_total += (data["obs"][0][1] / 10.0)
+                pres_total += (data["obs"][0][pres_idx] / 10.0)
                 self.report["pressure"] = float(pres_total) / float(air_count)
 
                 # Calculate dewpoint
-                b = (17.625 * data["obs"][0][2]) / (243.04 + data["obs"][0][2])
-                rh = float(data["obs"][0][3]) / 100.0
+                b = (17.625 * data["obs"][0][temp_idx]) / (243.04 + data["obs"][0][temp_idx])
+                rh = float(data["obs"][0][humd_idx]) / 100.0
                 c = math.log(rh)
                 dewpoint = (243.04 * (c + b)) / (17.625 - c - b)
                 dewp_total += dewpoint
                 self.report["dewpoint"] = dewp_total / air_count
 
                 # Track Min/Max
-                if (data["obs"][0][2] > self.report["max_temp"]):
-                    self.report["max_temp"] = data["obs"][0][2]
+                if (data["obs"][0][temp_idx] > self.report["max_temp"]):
+                    self.report["max_temp"] = data["obs"][0][temp_idx]
 
-                if (data["obs"][0][2] < self.report["min_temp"]):
-                    self.report["min_temp"] = data["obs"][0][2]
+                if (data["obs"][0][temp_idx] < self.report["min_temp"]):
+                    self.report["min_temp"] = data["obs"][0][temp_idx]
 
-                if (data["obs"][0][3] > self.report["max_humid"]):
-                    self.report["max_humid"] = data["obs"][0][3]
+                if (data["obs"][0][humd_idx] > self.report["max_humid"]):
+                    self.report["max_humid"] = data["obs"][0][humd_idx]
 
-                if (data["obs"][0][3] < self.report["min_humid"]):
-                    self.report["min_humid"] = data["obs"][0][3]
+                if (data["obs"][0][humd_idx] < self.report["min_humid"]):
+                    self.report["min_humid"] = data["obs"][0][humd_idx]
+
+                
+                debugMsg += "Temp (dF)= %.2f, " % ((float(data["obs"][0][temp_idx]) * 9 / 5) + 32)    # convert degC to degF
+                debugMsg += "Humid  = %.2f, " % data["obs"][0][humd_idx]
+                debugMsg += "Press (inHg) = %.2f, " % (float(data["obs"][0][pres_idx]) /  33.8639 )
+
+        
+            if  (   ((data["type"] == "obs_sky") and (data["serial_number"] == self.params["SkySerialNumber"])) or
+                    ((data["type"] == "obs_st") and (data["serial_number"] == self.params["TempestSerialNum"])) ):
+
+                if   data["type"] == "obs_sky":                  
+                    wind_idx = 5                # UDP  Packet data indexes for Sky device
+                    srad_idx = 10
+                    rain_idx = 11
+                else:
+                    wind_idx = 2                # UDP packet data indexes for TEMPEST device
+                    srad_idx = 11
+                    rain_idx = 12
 
 
-            if (data["type"] == "obs_sky") and (data["serial_number"] == self.params["SkySerialNumber"]):
                 sky_count += 1
 
-                wind_total += data["obs"][0][5]
+                wind_total += data["obs"][0][wind_idx]
                 self.report["wind"] = float(wind_total) / float(sky_count)
 
-                srad_total += data["obs"][0][10]
+                srad_total += data["obs"][0][srad_idx]
                 self.report["srad"] = float(srad_total) / float(sky_count)
 
-                rain_total += data["obs"][0][3]
+                rain_total += data["obs"][0][rain_idx]
                 self.report["rain"] = rain_total
 
+
+                debugMsg += "Cumulative Rain (inch) = %.2f" % (rain_total / 25.4)
+                log.debug(debugMsg)
+           
+ 
             ts = mod_time.mktime(now.timetuple()) + now.microsecond / 1e6
             self.parserData[0] = {
                     'ts': ts,
@@ -275,6 +338,7 @@ if __name__ == "__main__":
     p = WeatherFlow()
     p.params["AirSerialNumber"] = "AR-00000000"
     p.params["SkySerialNumber"] = "SK-00000000"
+    p.params["TempestSerialNum"] = "ST-00000000"
     while True:
         p.perform()
         time.sleep(120)
